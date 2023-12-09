4a8bd5a3dfd62fcc89b973293a0b7b980c1eab0f
Pavel23791 <140964820+Pavel23791@users.noreply.github.com>
2023-12-09 12:02:04 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.urlencoded({extends: false}));
+app.use(express.json());
+
+app.use('/', (req, res) => {
+    return res.status(200).json({
+        message: 'Hello from Full-CICD-Project using AWS, Docker, Jenkis, Github'
+    })
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running via port ${port}`);
+})
