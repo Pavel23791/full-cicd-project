ac99e6f997a1cf5ce976b21c925c39991df8a3b9
Pavel23791 <140964820+Pavel23791@users.noreply.github.com>
2023-12-16 17:57:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.use('/', (req, res) => {
     return res.status(200).json({
-        message: 'Welcome to Full-CICD-Project using AWS, Docker, Jenkis, Github'
+        message: 'Welcome to Full-CICD-Project using AWS, Docker, Jenkis, Github!!'
     })
 })
 
